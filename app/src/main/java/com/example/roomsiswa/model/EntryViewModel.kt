a3b2fe304c62bbe5adb8aefffcc6370ab3cc1506
raihanariaputrab <arpb.business@gmail.com>
2023-12-04 21:28:11 +0700

    create fun detail siswa & siswa

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -3,6 +3,7 @@ package com.example.roomsiswa.model
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.roomsiswa.RepositorySiswa.RepositoriSiswa
+import com.example.roomsiswa.data.Siswa
 
 
 data class UIStateSiswa(
@@ -15,4 +16,21 @@ data class DetailSiswa(
     val alamat: String = "",
     val telpon: String = "",
 )
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telepon = telpon,
+)
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telepon,
+)
+
 
