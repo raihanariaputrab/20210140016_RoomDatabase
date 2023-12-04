38a4f43345795311e50c02138aee6321aeee9557
raihanariaputrab <arpb.business@gmail.com>
2023-12-04 21:24:23 +0700

    create dataclass

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,2 +1,18 @@
 package com.example.roomsiswa.model
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.RepositorySiswa.RepositoriSiswa
+
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
