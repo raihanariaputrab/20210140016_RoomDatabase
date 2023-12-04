1950ed15df2ba499f78fbc58b91eccc8d0f1cc73
raihanariaputrab <arpb.business@gmail.com>
2023-12-04 21:35:09 +0700

    create private fun, suspend fun

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,9 +1,28 @@
 package com.example.roomsiswa.model
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.roomsiswa.RepositorySiswa.RepositoriSiswa
 import com.example.roomsiswa.data.Siswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+    private fun validasiInput(uiState : DetailSiswa = uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            nama.isNotBlank()&& alamat.isNotBlank()&& telpon.isNotBlank()
+        }
+    }
+    suspend fun saveSiswa(){
+        if (validasiInput()){
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
+
+
 
 
 data class UIStateSiswa(
