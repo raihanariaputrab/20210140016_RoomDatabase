4d4750c2ebe1f0dfbf5cf49b958f29d755d1bb47
raihanariaputrab <arpb.business@gmail.com>
2023-12-04 22:56:26 +0700

    create object destinasi

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,4 +1,21 @@
 package com.example.roomsiswa.model
 
-class PenyediaViewModel {
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswa.AplikasiSiswa
+
+object PenyediaViewModel {
+    val factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
 }
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
---
+++ app/src/main/java/com/example/roomsiswa/navigasi/DestinasiNavigasi.kt
@@ -0,0 +1,15 @@
+package com.example.roomsiswa.navigasi
+
+interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalur untuk composable
+     */
+    val route: String
+
+    /**
+     * String resource id yang berisi judul yang akan ditampilkan dilayar halaman
+     */
+
+    val titleRes: Int
+
+}
