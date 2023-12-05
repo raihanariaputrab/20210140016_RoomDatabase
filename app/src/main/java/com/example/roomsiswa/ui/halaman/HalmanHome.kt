e6570e406fd9502d6cf17d1be735c99eb0ac94f9
raihanariaputrab <arpb.business@gmail.com>
2023-12-05 08:48:49 +0700

    add fun home screen

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalmanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalmanHome.kt
@@ -1,9 +1,22 @@
 package com.example.roomsiswa.ui.halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomsiswa.R
+import com.example.roomsiswa.model.HomeViewModel
+import com.example.roomsiswa.model.PenyediaViewModel
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
 
 object DestinasiHome: DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
+}
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
 }
